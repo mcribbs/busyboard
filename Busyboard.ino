abbd23d21dcf7b8e92305c75418da8d2a2fc3749
Matt Cribbs <mcribbs@gmail.com>
2017-03-05 00:40:37 -0500

    Setup and test fully build hardware

--- Busyboard.ino
+++ Busyboard.ino
@@ -1,25 +1,20 @@
-const int SWITCH_A = 28;
-const int SWITCH_B = 30;
-const int SWITCH_C = 32;
-const int SWITCH_D = 29;
-const int SWITCH_E = A14;
-const int SWITCH_F = A15;
-const int LED_A = 22;
-const int LED_B = 24;
-const int LED_C = 26;
-const int LED_D = 23;
-const int LED_E = 25;
-const int LED_F = 27;
+// Define input pins
+const int SWITCH[] = {13, 12, 11};
+const int BUTTON[] = {A8, A9, 10};
+const int BUTTON_ANALOG[] = {true, true, false};
 
-int stateA = 0;
-int stateB = 0;
-int stateC = 0;
-int stateD = 0;
-int stateE = 0;
-int stateF = 0;
+// Define output pins
+const int LED_RED[] = {22, 23, 24, 25, 26};
+const int LED_YEL[] = {27, 28, 29, 30, 31};
+const int LED_BLU[] = {32, 33, 34, 35, 36};
+const int LED_SAG[] = {39, 40, 41, 38, 37, 44, 45, 46, 47, 48, 49, 50, 42, 43};
+const int SPEAKER = 8;
+
+// Input state trackers
+int switch_state[] = {0, 0, 0};
+int button_state[] = {0, 0, 0};
 
 #include "pitches.h"
-
 // notes in the melody:
 int melody[] = {
   NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
@@ -33,92 +28,70 @@ int noteDurations[] = {
 
 void setup() {
   Serial.begin(9600);
+
+  // Setup inputs
+  for (int i = 0; i < 3; i++) {
+    pinMode(SWITCH[i], INPUT);
+    if (!BUTTON_ANALOG[i]) {
+       pinMode(BUTTON[i], INPUT);
+    }
+  }
   
-  pinMode(SWITCH_A, INPUT);
-  pinMode(SWITCH_B, INPUT);
-  pinMode(SWITCH_C, INPUT);
-  pinMode(SWITCH_D, INPUT);
-  //pinMode(SWITCH_E, INPUT);
-  //pinMode(SWITCH_F, INPUT);
-        
-  pinMode(LED_A, OUTPUT);
-  pinMode(LED_B, OUTPUT);
-  pinMode(LED_C, OUTPUT);
+  // Setup outputs
+  for (int i = 0; i < 5; i++) {
+    pinMode(LED_RED[i], OUTPUT);
+    pinMode(LED_YEL[i], OUTPUT);
+    pinMode(LED_BLU[i], OUTPUT);
+  }
+  for (int i = 0; i < 14; i++) {
+    pinMode(LED_SAG[i], OUTPUT);
+  }
 
   // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+  for (int thisNote = 0; thisNote < SPEAKER; thisNote++) {
 
     // to calculate the note duration, take one second
     // divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / noteDurations[thisNote];
-    tone(8, melody[thisNote], noteDuration);
+    tone(SPEAKER, melody[thisNote], noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
-    noTone(8);
+    noTone(SPEAKER);
   }
 }
 
 void loop() {
-  stateA = digitalRead(SWITCH_A);
-  stateB = digitalRead(SWITCH_B);
-  stateC = digitalRead(SWITCH_C);
-  stateD = digitalRead(SWITCH_D);
-  stateE = analogRead(SWITCH_E);
-  stateF = analogRead(SWITCH_F);
-  
-  Serial.print(stateA);
-  Serial.print(", ");
-  Serial.print(stateB);
-  Serial.print(", ");
-  Serial.print(stateC);
-  Serial.print(", ");
-  Serial.print(stateD);
-  Serial.print(", ");
-  Serial.print(stateE);
-  Serial.print(", ");
-  Serial.print(stateF);
-  Serial.println();
-  
-  if (stateA == LOW) {
-    digitalWrite(LED_A, HIGH);
-  } else {
-    digitalWrite(LED_A, LOW);
+  for (int i = 0; i < 3; i++) {
+    switch_state[i] = digitalRead(SWITCH[i]);
+    if (!BUTTON_ANALOG[i]) {
+       button_state[i] = digitalRead(BUTTON[i]);
+    } else {
+      button_state[i] = analogRead(BUTTON[i]) < 5 ? LOW : HIGH;  
+    }
   }
 
-  if (stateB == LOW) {
-    digitalWrite(LED_B, HIGH);   
-  } else {
-    digitalWrite(LED_B, LOW);   
+  /*
+  Serial.print(switch_state[0]); Serial.print(", ");
+  Serial.print(switch_state[1]); Serial.print(", ");
+  Serial.print(switch_state[2]); Serial.print(", ");
+  Serial.print(button_state[0]); Serial.print(", ");
+  Serial.print(button_state[1]); Serial.print(", ");
+  Serial.print(button_state[2]); Serial.println();
+  */
+  
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(LED_BLU[i], !switch_state[0] ? HIGH : LOW);
+    digitalWrite(LED_YEL[i], !switch_state[1] ? HIGH : LOW);
+    digitalWrite(LED_RED[i], !switch_state[2] ? HIGH : LOW);
   }
-
-  if (stateC == LOW) {
-    digitalWrite(LED_C, HIGH);  
-  } else {
-    digitalWrite(LED_C, LOW);   
-  }  
-
-  if (stateD == LOW) {
-    digitalWrite(LED_D, HIGH);  
-  } else {
-    digitalWrite(LED_D, LOW);   
-  }  
-
-  if (stateE < 5) {
-    digitalWrite(LED_E, HIGH);  
-  } else {
-    digitalWrite(LED_E, LOW);   
-  }  
-
-  if (stateF < 5) {
-    digitalWrite(LED_F, HIGH);  
-  } else {
-    digitalWrite(LED_F, LOW);   
-  }  
+  for (int i = 0; i < 14; i++) {
+    digitalWrite(LED_SAG[i], !button_state[0] ? HIGH : LOW);
+  }
 
   delay(1);
 }
